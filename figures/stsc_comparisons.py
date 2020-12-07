5d88d68ac2e9d263a697332b8a152f2cb31a43ce
Jonathan Yong <yongrenjie@gmail.com>
2020-12-07 14:59:15 +0000

    Finish HSQC-TOCSY section in SI

--- figures/stsc_comparisons.py
+++ figures/stsc_comparisons.py
@@ -6,24 +6,33 @@ from penguins.private import Andrographolide as Andro
 import matplotlib.pyplot as plt
 plt.style.use(Path(__file__).parent / "helv.mplstyle")
 
-path = nmrd() / "201010-7a-hsqctocsy"
+# Just change these...
+path = nmrd() / "201206-7a-hsqct-full"
+ref_noah_sc_expno = 3
+ssc_expnos = range(11, 19)
+sspc_expnos = range(19, 27)
+stsc_expnos = range(27, 35)
+stspc_expnos = range(35, 43)
+
 
 # Read reference datasets and get reference intensities
-ref_s, ref_c = [pg.read(path, expno) for expno in (1001, 1002)]
+ref_s, ref_c = [pg.read(path, expno)
+                for expno in (ref_noah_sc_expno * 1000 + 1,
+                              ref_noah_sc_expno * 1000 + 2)]
 ref_s_ints = Andro.hsqc.integrate(ref_s)
 ref_c_ints = Andro.cosy.integrate(ref_c)
 
 # Read in datasets of interest
-noah_ssc_s = [pg.read(path, expno * 1000 + 2) for expno in range(11, 18)]
-noah_ssc_c = [pg.read(path, expno * 1000 + 3) for expno in range(11, 18)]
-noah_stsc_s = [pg.read(path, expno * 1000 + 2) for expno in range(21, 28)]
-noah_stsc_c = [pg.read(path, expno * 1000 + 3) for expno in range(21, 28)]
+noah_ssc_s = [pg.read(path, expno * 1000 + 2) for expno in ssc_expnos]
+noah_ssc_c = [pg.read(path, expno * 1000 + 3) for expno in ssc_expnos]
+noah_stsc_s = [pg.read(path, expno * 1000 + 2) for expno in stsc_expnos]
+noah_stsc_c = [pg.read(path, expno * 1000 + 3) for expno in stsc_expnos]
 
 # seHSQC in slot 2
-noah_sspc_sp = [pg.read(path, expno * 1000 + 2) for expno in range(31, 38)]
-noah_sspc_c = [pg.read(path, expno * 1000 + 3) for expno in range(31, 38)]
-noah_stspc_sp = [pg.read(path, expno * 1000 + 2) for expno in range(41, 48)]
-noah_stspc_c = [pg.read(path, expno * 1000 + 3) for expno in range(41, 48)]
+noah_sspc_sp = [pg.read(path, expno * 1000 + 2) for expno in sspc_expnos]
+noah_sspc_c = [pg.read(path, expno * 1000 + 3) for expno in sspc_expnos]
+noah_stspc_sp = [pg.read(path, expno * 1000 + 2) for expno in stspc_expnos]
+noah_stspc_c = [pg.read(path, expno * 1000 + 3) for expno in stspc_expnos]
 
 # Calculate intensities (averaged over all peaks) for datasets of interest
 get_rel_s_ints = lambda ds: np.mean(Andro.hsqc.integrate(ds) / ref_s_ints)
@@ -54,22 +63,25 @@ for (ax,
                            [noah_stsc_s_ints, noah_stspc_sp_ints],
                            [noah_ssc_c_ints, noah_sspc_c_ints],
                            [noah_stsc_c_ints, noah_stspc_c_ints],
-                           ["HSQC", "seHSQC"],
-                           ["NOAH-3 StSCc", "NOAH-3 StSpCc"]):
+                           ["S", "Sp"],
+                           ["With HSQC as second module", "With seHSQC as second module"]):
     # Plot NOAH intensities
-    ax.plot(cnst32s, s1_ints, linestyle="--", linewidth=0.5, color=pastel[1])
-    ax.plot(cnst32s, s2_ints, color=deep[1])
-    ax.plot(cnst32s, c1_ints, linestyle="--", linewidth=0.5, color=pastel[2])
-    ax.plot(cnst32s, c2_ints, color=deep[2])
+    ax.plot(cnst32s, s1_ints, linestyle="--", color=pastel[1], marker="o")
+    ax.plot(cnst32s, s2_ints, color=deep[1], marker="o")
+    ax.plot(cnst32s, c1_ints, linestyle="--", color=pastel[2], marker="x")
+    ax.plot(cnst32s, c2_ints, color=deep[2], marker="x")
     # Twiddle with axes properties.
-    ax.set(xlabel="value of $f$", ylabel="Relative intensity",
+    ax.set(xlabel="value of $f$",
+           ylabel="Intensity relative to NOAH-2 SCc",
            ylim=(-0.05, 1.15), title=title)
-    ax.legend(["", f"{label2} #2", "", "COSY"], loc="lower right")
+    ax.legend([rf"S$\bf {label2}$Cc", rf"St$\bf {label2}$Cc",
+               rf"S{label2}$\bf Cc$", rf"St{label2}$\bf Cc$",],
+              loc="lower right")
     ax.invert_xaxis()
     ax.label_outer()
     pg.style_axes(ax, "plot")
 
 pg.label_axes(axs, fstr="({})", fontweight="bold", fontsize=14)
-pg.show()
-# for filetype in [".png", ".svg"]:
-#     pg.savefig(str(Path(__file__)).replace(".py", filetype))
+# pg.show()
+for filetype in [".png", ".svg"]:
+    pg.savefig(str(Path(__file__)).replace(".py", filetype))
