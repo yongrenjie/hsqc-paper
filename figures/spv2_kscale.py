Fix k-scaling figures to include SW1 scaling

figures/spv2_kscale.py
@@ -5,53 +5,79 @@ from penguins.private import nmrd
 import matplotlib.pyplot as plt
 plt.style.use(Path(__file__).parent / "helv.mplstyle")
 
-path = nmrd() / "201017-7g-n15-sehsqc-full"
-ks = [1, 2, 4, 8]
-expnos = [42001, 43001, 44001, 45001]
-dss = [pg.read(path, expno) for expno in expnos]
+path = nmrd() / "210310-7g-n15-sw1"
+dss = [
+    pg.read(path, 101001, 1),  # normal
+    pg.read(path, 102001, 1),  # k = 2 no LP
+    pg.read(path, 103001, 1),  # k = 4 no LP
+    pg.read(path, 104001, 1),  # k = 8 no LP
+    pg.read(path, 105001, 1),  # SW * 2 no LP
+    pg.read(path, 106001, 1),  # SW * 4 no LP
+    pg.read(path, 107001, 1),  # SW * 8 no LP
+]
+titles = [
+    r"standard ($k = 1$, SW = 30 ppm)",
+    r"$k = 2$", r"$k = 4$", r"$k = 8$",
+    "SW = 60 ppm", "SW = 120 ppm", "SW = 240 ppm"
+]
 
-#######################################################################
-# If we change to 2x4 then only this block of code needs to be modified.
-#######################################################################
-fig, axs = pg.subplots2d(4, 2, figsize=(8, 11.2),
-                       gridspec_kw={"height_ratios": [2, 2, 0.8, 0.8]})
-# Make these flat lists of the relevant axes, and the rest of the code will
-# work.
-axes_2d = [axs[0][0], axs[0][1], axs[1][0], axs[1][1]]
-axes_1d = [axs[2][0], axs[2][1], axs[3][0], axs[3][1]]
-#######################################################################
+fig = plt.figure(figsize=(16, 12), constrained_layout=True)
+gs = fig.add_gridspec(ncols=16, nrows=12)
+axs = [
+    fig.add_subplot(gs[2:6, 0:4]),
+    fig.add_subplot(gs[0:4, 4:8]), fig.add_subplot(gs[0:4, 8:12]), fig.add_subplot(gs[0:4, 12:16]),
+    fig.add_subplot(gs[4:8, 4:8]), fig.add_subplot(gs[4:8, 8:12]), fig.add_subplot(gs[4:8, 12:16]),
+    fig.add_subplot(gs[9:11, 0:4]),
+    fig.add_subplot(gs[8:10, 4:8]), fig.add_subplot(gs[8:10, 8:12]), fig.add_subplot(gs[8:10, 12:16]),
+    fig.add_subplot(gs[10:12, 4:8]), fig.add_subplot(gs[10:12, 8:12]), fig.add_subplot(gs[10:12, 12:16])
+]
 
-# Plot 2Ds
-for ds, ax, k in zip(dss, axes_2d, ks):
-    ds.stage(ax, levels=5e3, f1_bounds="110..130", f2_bounds="7..9.3")
-    pg.mkplot(ax, title=f"seHSQC, $k$ = {k}")
-    pg.move_ylabel(ax, pos="topright")
+f1b, f2b = "110..131", "7..9.3"
 
-# Plot 1D projections
-for ds, ax, k in zip(dss, axes_1d, ks):
-    ds.f1projp().stage(ax, bounds="110..132")
-    pg.mkplot(ax)
-# Set ylims to be the same
-ymin, ymax = axes_1d[3].get_ylim()
-for ax in axes_1d:
-    ax.set_ylim((ymin, ymax))
+# Plot data.
+for ds, ax, title in zip(dss, axs[:7], titles):
+    ds.stage(ax, levels=5e3, f1_bounds=f1b, f2_bounds=f2b)
+    pg.mkplot(ax, title=title, tight_layout=False)
+    pg.move_ylabel(ax, pos="topright", tight_layout=False)
+for ds, ax in zip(dss, axs[7:]):
+    ds.f1projp().stage(ax, bounds=f1b)
+    pg.mkplot(ax, tight_layout=False)
+# Standardise ylims of projections.
+ymin, ymax = axs[10].get_ylim()
+for ax in axs[7:]:
+    ax.set_ylim(ymin, ymax*1.1)
+
 # Calculate and display integrals
 shifts = (113.23, 123.27, 125.43, 128.02)
 margin = 0.2
-ref_ints = np.array([dss[0].f1projp().integrate(peak=shift, margin=margin, mode="max")
-                      for shift in shifts])
-for ds, ax in zip(dss[1:], axes_1d[1:]):
-    abs_ints = np.array([ds.f1projp().integrate(peak=shift, margin=margin, mode="max")
-                         for shift in shifts])
-    rel_ints = abs_ints / ref_ints
-    for shift, abs_integ, rel_integ in zip(shifts, abs_ints, rel_ints):
-        voffset = 2e3
-        ax.text(x=shift, y=abs_integ+voffset, s=f"{rel_integ:.1f}×",
+get_integs = lambda ds: np.array([ds.f1projp().integrate(peak=shift,
+                                                         margin=margin,
+                                                         mode="max")
+                                  for shift in shifts])
+ref_integs = get_integs(dss[0])
+for ds, ax in zip(dss[1:], axs[8:]):
+    integs = get_integs(ds)
+    rel_integs = integs / ref_integs
+    for shift, integ, rel_integ in zip(shifts, integs, rel_integs):
+        voffset = (ymax*1.1 - ymin) * 0.05
+        ax.text(x=shift, y=integ+voffset, s=f"{rel_integ:.1f}×",
                 fontsize=9, horizontalalignment="center")
 
-pg.cleanup_axes()
-pg.label_axes(axs, fstr="({})", fontweight="bold", fontsize=14)
+# Tidy up
+pg.label_axes(axs[0:7], fstr="({})", fontweight="bold", fontsize=14)
+pg.label_axes(axs[7:14], start=8, fstr="({})", fontweight="bold", fontsize=14,
+              offset=(0.02, 0.05))
 
+# Move the first Axes down a bit.
+fig.execute_constrained_layout()    # This is needed not only for moving
+                                    # the Axes, but also for pg.cleanup_axes().
+                                    # Reasons unclear.
+first_ax_bbox = axs[0].get_position()
+first_ax_bbox.y0 = first_ax_bbox.y0 - 0.07
+first_ax_bbox.y1 = first_ax_bbox.y1 - 0.07
+axs[0].set_position(first_ax_bbox)
+
+pg.cleanup_axes()
 # pg.show()
 for filetype in [".png", ".svg"]:
     pg.savefig(str(Path(__file__)).replace(".py", filetype))