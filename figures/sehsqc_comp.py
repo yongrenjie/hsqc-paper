8d92921678840bcb8d88df10b4dcba5735f46201
Jonathan Yong <yongrenjie@gmail.com>
2021-03-19 02:17:22 +0000

    Update main text and SI, almost done

--- figures/sehsqc_comp.py
+++ figures/sehsqc_comp.py
@@ -19,16 +19,34 @@ spv1c_c = pg.read(p, 21002)
 spv2c_s = pg.read(p, 23001)
 spv2c_c = pg.read(p, 23002)
 
-fig, axs = plt.subplots(2, 3, figsize=(6.5, 6),
-                        gridspec_kw={"height_ratios": [2, 1]}
-                        )
+fig = plt.figure(figsize=(6.5, 7.5))
+gs = fig.add_gridspec(3, 3, height_ratios=[0.8, 2, 0.8])
+axs = [fig.add_subplot(gs[0, :]),
+       fig.add_subplot(gs[1, 0]),
+       fig.add_subplot(gs[1, 1]),
+       fig.add_subplot(gs[1, 2]),
+       fig.add_subplot(gs[2, 0]),
+       fig.add_subplot(gs[2, 1]),
+       fig.add_subplot(gs[2, 2]),
+       ]
+
+# Pulse programme (imported from Inkscape)
+pprogs_clipcosy = plt.imread(Path(__file__).parent / "pprogs_clipcosy.png")
+axs[0].imshow(pprogs_clipcosy)
+axs[0].xaxis.set_visible(False)
+axs[0].yaxis.set_visible(False)
+axs[0].spines["top"].set_visible(False)
+axs[0].spines["left"].set_visible(False)
+axs[0].spines["right"].set_visible(False)
+axs[0].spines["bottom"].set_visible(False)
+
 # CRK seHSQC vs standard HSQC
 hsqc_cosy_stripplot(molecule=Andro,
                     datasets=[sporc_s, sporc_c],
                     ref_datasets=[sc_s, sc_c],
                     xlabel=r"NOAH-2 $\rm S^{+}_{crk}C^c$",
                     ylabel=r"intensity vs NOAH-2 $\rm SC^c$",
-                    ax=axs[0][0],
+                    ax=axs[1],
                     size=4,
                     font_kwargs={"size": 8.5}
                     )
@@ -38,7 +56,7 @@ hsqc_cosy_stripplot(molecule=Andro,
                     ref_datasets=[sc_s, sc_c],
                     xlabel=r"NOAH-2 $\rm S^{+}_{1}C^c$",
                     ylabel=r"intensity vs NOAH-2 $\rm SC^c$",
-                    ax=axs[0][1],
+                    ax=axs[2],
                     size=4,
                     font_kwargs={"size": 8.5}
                     )
@@ -48,57 +66,69 @@ hsqc_cosy_stripplot(molecule=Andro,
                     ref_datasets=[sc_s, sc_c],
                     xlabel=r"NOAH-2 $\rm S^{+}_{2}C^c$",
                     ylabel=r"intensity vs NOAH-2 $\rm SC^c$",
-                    ax=axs[0][2],
+                    ax=axs[3],
                     size=4,
                     font_kwargs={"size": 8.5}
                     )
-axs[0][1].yaxis.set_visible(False)
-axs[0][2].yaxis.set_visible(False)
-for ax in axs[0]:
+axs[2].yaxis.set_visible(False)
+axs[3].yaxis.set_visible(False)
+for ax in axs[1:4]:
     ax.legend().set_visible(False)
     ax.set_xlim(-0.4, 0.4)
     ax.set_ylim(-0.3, 2.4)
     pg.style_axes(ax, "plot")
 
 
-handles, labels = ax.get_legend_handles_labels()
+handles, labels = axs[3].get_legend_handles_labels()
 for handle in handles[0:4]:
     handle.set(sizes=[15])
 fig.legend(handles[0:4], ["HSQC CH", "HSQC CH$_2$", "HSQC CH$_3$", "COSY"],
            ncol=4, fontsize=10,
-           bbox_to_anchor=(0.54, 0.99),
+           bbox_to_anchor=(0.54, 0.81),
            loc="upper center")
 
 # Projections
 dark = pg.color_palette("dark")
 bright = pg.color_palette("bright")
 # CH
-b = (3.08, 3.38)
+b = (3.09, 3.39)
 c1, c2 = dark[0], bright[0]
 sp_slice = spv2c_s.slice(axis=1, ppm=78.9)
 s_slice = sc_s.slice(axis=1, ppm=78.9)
-s_slice.stage(ax=axs[1][0], bounds=b, linestyle="--", color=c2)
-sp_slice.stage(ax=axs[1][0], bounds=b, color=c1)
+s_slice.stage(ax=axs[4], bounds=b, linestyle="--", color=c2)
+sp_slice.stage(ax=axs[4], bounds=b, color=c1)
 s_int = s_slice.integrate(bounds=b, mode="max")
 sp_int = sp_slice.integrate(bounds=b, mode="max")
-axs[1][0].text(x=3.23-0.03, y=s_int, s="HSQC", color=c2, horizontalalignment="left",
+axs[4].text(x=0.02, y=0.8,
+            s="HSQC CH", color='black', horizontalalignment="left",
+            verticalalignment="top", fontsize=10,
+            transform=axs[4].transAxes)
+axs[4].text(x=3.23-0.03, y=s_int,
+            s=r"$\rm SC^{c}$", color=c2, horizontalalignment="left",
             verticalalignment="top", fontsize=8)
-axs[1][0].text(x=3.23-0.02, y=sp_int, s=r"ZIP-seHSQC ($\rm S^{+}_{2}$)", color=c1, horizontalalignment="left",
-            verticalalignment="top", fontsize=8)
-axs[1][0].text(x=3.23-0.02, y=sp_int-1.8e4, s=f"({sp_int/s_int:.2f}×)",
+axs[4].text(x=3.23-0.02, y=sp_int,
+            s=(r"$\rm S^{+}_{2}C^{c}$" + f" ({sp_int/s_int:.2f}×)"),
             color=c1, horizontalalignment="left",
             verticalalignment="top", fontsize=8)
 
 # CH2
-b = (1.49, 1.79)
+b = (1.52, 1.82)
 c1, c2 = dark[1], bright[1]
 sp_slice = spv2c_s.slice(axis=1, ppm=28.45)
 s_slice = sc_s.slice(axis=1, ppm=28.45)
-s_slice.stage(ax=axs[1][1], bounds=b, linestyle="--", color=c2)
-sp_slice.stage(ax=axs[1][1], bounds=b, color=c1)
+s_slice.stage(ax=axs[5], bounds=b, linestyle="--", color=c2)
+sp_slice.stage(ax=axs[5], bounds=b, color=c1)
 s_int = s_slice.integrate(bounds=b, mode="max")
 sp_int = sp_slice.integrate(bounds=b, mode="max")
-axs[1][1].text(x=1.64-0.02, y=sp_int, s=f"({sp_int/s_int:.2f}×)",
+axs[5].text(x=0.02, y=0.8,
+            s=r"HSQC $\rm CH_2$", color='black', horizontalalignment="left",
+            verticalalignment="top", fontsize=10,
+            transform=axs[5].transAxes)
+axs[5].text(x=1.65-0.03, y=s_int,
+            s=r"$\rm SC^{c}$", color=c2, horizontalalignment="left",
+            verticalalignment="top", fontsize=8)
+axs[5].text(x=1.65-0.02, y=sp_int,
+            s=(r"$\rm S^{+}_{2}C^{c}$" + f" ({sp_int/s_int:.2f}×)"),
             color=c1, horizontalalignment="left",
             verticalalignment="top", fontsize=8)
 
@@ -107,22 +137,31 @@ b = (1.65, 2.05)
 c1, c2 = dark[3], bright[3]
 sp_slice = spv2c_c.slice(axis=1, ppm=1.36)
 s_slice = sc_c.slice(axis=1, ppm=1.36)
-s_slice.stage(ax=axs[1][2], bounds=b, linestyle="--", color=c2)
-sp_slice.stage(ax=axs[1][2], bounds=b, color=c1)
+s_slice.stage(ax=axs[6], bounds=b, linestyle="--", color=c2)
+sp_slice.stage(ax=axs[6], bounds=b, color=c1)
 s_int1 = s_slice.integrate(bounds=(1.72, 1.76), mode="max")
 s_int2 = s_slice.integrate(bounds=(1.92, 1.96), mode="max")
 sp_int1 = sp_slice.integrate(bounds=(1.72, 1.76), mode="max")
 sp_int2 = sp_slice.integrate(bounds=(1.92, 1.96), mode="max")
-axs[1][2].text(x=1.74-0.02, y=sp_int1-2e4, s=f"({sp_int1/s_int1:.2f}×)",
+axs[6].text(x=0.02, y=0.8,
+            s="COSY", color='black', horizontalalignment="left",
+            verticalalignment="top", fontsize=10,
+            transform=axs[6].transAxes)
+axs[6].text(x=1.74-0.02, y=sp_int1-2e4, s=f"({sp_int1/s_int1:.2f}×)",
             color=c1, horizontalalignment="left",
             verticalalignment="top", fontsize=8)
-axs[1][2].text(x=1.94+0.02, y=sp_int2-2e4, s=f"({sp_int2/s_int2:.2f}×)",
-            color=c1, horizontalalignment="right",
+axs[6].text(x=1.93-0.01, y=s_int2+1e5,
+            s=r"$\rm SC^{c}$", color=c2, horizontalalignment="left",
+            verticalalignment="top", fontsize=8)
+axs[6].text(x=1.93-0.02, y=sp_int2-2e4,
+            s=(r"$\rm S^{+}_{2}C^{c}$" + f"\n({sp_int2/s_int2:.2f}×)"),
+            color=c1, horizontalalignment="left",
             verticalalignment="top", fontsize=8)
 
-pg.mkplots(axs[1])
-pg.label_axes(axs, fstr="({})", fontsize=14, fontweight="bold")
-pg.cleanup_figure()
+pg.mkplots(axs[4:])
+pg.label_axes(axs[0], fstr="({})", fontsize=14, fontweight="bold",
+              offset=(0.005, -0.05))
+pg.label_axes(axs[1:], start=2, fstr="({})", fontsize=14, fontweight="bold")
 
 # pg.show()
 for filetype in [".png", ".svg"]:
